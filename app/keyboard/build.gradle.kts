5d6ee807416b9d79c04b3450b5310231e4cf49ae
particleyh <98777973+particleyh@users.noreply.github.com>
2026-09-09 05:30:33 +0300

    chore: bump version to 5.9 (versionCode 59)

--- app/keyboard/build.gradle.kts
+++ app/keyboard/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.keyboard.calligraphy"
         minSdk = 24
         targetSdk = 36
-        versionCode = 58
-        versionName = "5.8"
+        versionCode = 59
+        versionName = "5.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
